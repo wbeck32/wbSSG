5ce6f0c819609be4b8cf4929c5358abfcb8f2f23
Wendy Beck <webeck@gmail.com>
2015-01-27 14:30:50 -0800

    updated projects and jade files

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,21 +1,24 @@
 (function(){
+
+
 //need jquery, node
 
-//var cSide = $.ajax({/
-//	url: 'http://localhost:3000/'
-//})
-//.done(function(data){/
-//	console.log(data);
-	//console.log(cSide);
-//});
+var cSide = $.ajax({
+	url: 'http://localhost:3000/home'
+})
+.done(function(data) {
+	console.log(data);
+	//data.join('')
+	$(data).appendTo('#projects');
+//	return data;
+});
 //console.log(cSide);
 
 //create new project div
-//var projDiv
 
 //for each file in project
-	//output contents of html file
-	//projDiv += contents
+//	output contents of html file
+//	projDiv += contents
 //end foreach
 
 //append projDiv to main 
